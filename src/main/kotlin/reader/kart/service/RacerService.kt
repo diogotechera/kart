e90fdd9e5447959cfa3195911f0decc879281c43
Diogo Techera <diogo.techera@wirecard.com>
2019-08-04 23:27:56 -0300

    refactor raceService

--- src/main/kotlin/reader/kart/service/RacerService.kt
+++ src/main/kotlin/reader/kart/service/RacerService.kt
@@ -7,8 +7,6 @@ import reader.kart.repository.RacerRepository
 @Service
 class RacerService(val racerRepository: RacerRepository) {
 
-    fun save(racer: Racer): Racer {
-        return racerRepository.find(racer.code) ?: racerRepository.save(racer)
-    }
+    fun save(racer: Racer) = racerRepository.find(racer.code) ?: racerRepository.save(racer)
 
 }
